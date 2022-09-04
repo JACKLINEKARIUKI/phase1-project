294428fff573a5be952d73a3c8805966468645ec
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-09-04 15:52:30 +0300

    create function to add to wishlist

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,13 +17,14 @@ const books = {
         9780399160684, 9782724232363, 9780395779279],
     literature : [ 9780451526380, 9781596881976, 9781517622770, 9780679203742,
         9780143039563, 9780007420063, 9780679601685, 9780140434156],
-    trending : [9780525427384, 9780385514231, 9789636893316,
+    thriller : [9780679444817, 9780385514231, 9789636893316,
         9781443431255, 9781844138548, 9781410452580, 0425130266, 9780754014362],
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     categoriesMenu();
     selectMenu();
+    addToWishlist();
     
 });
 
@@ -48,8 +49,8 @@ function selectMenu(books){
                 fetchHorrorBooks(books);
             } else if (e.target.textContent === 'Literature') {
                 fetchLiteratureBooks(books);
-            } else if (e.target.textContent === 'Trending') {
-                fetchTrendingBooks(books);
+            } else if (e.target.textContent === 'Thriller') {
+                fetchThrillerBooks(books);
             }
         })
 
@@ -94,7 +95,8 @@ function fetchFantasyBooks(){
 
 
 
-function fetchHorrorBooks(books){
+function fetchHorrorBooks(){
+    const main = document.querySelector('main');
     main.innerHTML = `<div class="categories"></div>`;
     const categoryName = document.createElement('div');
     categoryName.className = 'category';
@@ -104,17 +106,18 @@ function fetchHorrorBooks(books){
     categoryName.appendChild(booksList);
     main.appendChild(categoryName);
 
-    for (let i = 0; i < books.fantasy.length; i++) {
+    for (let i = 0; i < books.horror.length; i++) {
         fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${books.horror[i]}&jscmd=data&format=json`)
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             const bookCard = document.createElement('div');
             bookCard.className = 'book-card';
             bookCard.innerHTML = `
-            <img src="${data[`ISBN:${books.fantasy[i]}`].cover.large}">
+            <img src="${data[`ISBN:${books.horror[i]}`].cover.large}">
             <div class="book-detail">
-                <h3>${data[`ISBN:${books.fantasy[i]}`].title}</h3>
-                <h4>${data[`ISBN:${books.fantasy[i]}`].authors[0].name}</h4>
+                <h3>${data[`ISBN:${books.horror[i]}`].title}</h3>
+                <h4>${data[`ISBN:${books.horror[i]}`].authors[0].name}</h4>
                 <div>
                 <i class="fa fa-heart-o" aria-hidden="true"></i>
                 <p class="hide">Add to wish list</p>
@@ -128,7 +131,7 @@ function fetchHorrorBooks(books){
     }
 }
 
-function fetchLiteratureBooks(books){
+function fetchLiteratureBooks(){
     const main = document.querySelector('main');
     main.innerHTML = `<div class="categories"></div>`;
     const categoryName = document.createElement('div');
@@ -139,17 +142,17 @@ function fetchLiteratureBooks(books){
     categoryName.appendChild(booksList);
     main.appendChild(categoryName);
 
-    for (let i = 0; i < books.fantasy.length; i++) {
+    for (let i = 0; i < books.literature.length; i++) {
         fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${books.literature[i]}&jscmd=data&format=json`)
         .then(response => response.json())
         .then(data => {
             const bookCard = document.createElement('div');
             bookCard.className = 'book-card';
             bookCard.innerHTML = `
-            <img src="${data[`ISBN:${books.fantasy[i]}`].cover.large}">
+            <img src="${data[`ISBN:${books.literature[i]}`].cover.large}">
             <div class="book-detail">
-                <h3>${data[`ISBN:${books.fantasy[i]}`].title}</h3>
-                <h4>${data[`ISBN:${books.fantasy[i]}`].authors[0].name}</h4>
+                <h3>${data[`ISBN:${books.literature[i]}`].title}</h3>
+                <h4>${data[`ISBN:${books.literature[i]}`].authors[0].name}</h4>
                 <div>
                 <i class="fa fa-heart-o" aria-hidden="true"></i>
                 <p class="hide">Add to wish list</p>
@@ -164,28 +167,29 @@ function fetchLiteratureBooks(books){
     
 };
 
-function fetchTrendingBooks(books){
+function fetchThrillerBooks(){
     const main = document.querySelector('main');
     main.innerHTML = `<div class="categories"></div>`;
     const categoryName = document.createElement('div');
     categoryName.className = 'category';
-    categoryName.innerHTML = `<h1>Trending</h1>`;
+    categoryName.innerHTML = `<h1>Thriller</h1>`;
     const booksList = document.createElement('div');
     booksList.className = 'books-list';
     categoryName.appendChild(booksList);
     main.appendChild(categoryName);
+    
 
-    for (let i = 0; i < books.fantasy.length; i++) {
-        fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${books.trending[i]}&jscmd=data&format=json`)
+    for (let i = 0; i < books.thriller.length; i++) {
+        fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${books.thriller[i]}&jscmd=data&format=json`)
         .then(response => response.json())
         .then(data => {
             const bookCard = document.createElement('div');
             bookCard.className = 'book-card';
             bookCard.innerHTML = `
-            <img src="${data[`ISBN:${books.fantasy[i]}`].cover.large}">
+            <img src="${data[`ISBN:${books.thriller[i]}`].cover.large}">
             <div class="book-detail">
-                <h3>${data[`ISBN:${books.fantasy[i]}`].title}</h3>
-                <h4>${data[`ISBN:${books.fantasy[i]}`].authors[0].name}</h4>
+                <h3>${data[`ISBN:${books.thriller[i]}`].title}</h3>
+                <h4>${data[`ISBN:${books.thriller[i]}`].authors[0].name}</h4>
                 <div>
                 <i class="fa fa-heart-o" aria-hidden="true"></i>
                 <p class="hide">Add to wish list</p>
@@ -199,6 +203,21 @@ function fetchTrendingBooks(books){
     }
 };
         
-
+function addToWishlist (){
+    const addWish = document.querySelectorAll('.wishlist-details i');
+    addWish.forEach(addWish => {
+        addWish.addEventListener('click', () => {
+            if (addWish.className === 'fa fa-heart-o'){
+                addWish.className = 'fa fa-heart';
+                addWish.nextElementSibling.className = 'hide1';
+            }
+            else if (addWish.className === 'fa fa-heart'){
+                addWish.className = 'fa fa-heart-o';
+                addWish.nextElementSibling.className = 'hide';
+            }
+    
+            });
+        }
+        )};
 
 
